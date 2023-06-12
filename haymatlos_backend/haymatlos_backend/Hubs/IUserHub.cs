8a96ed684f65ebbef3550a703c4f7f74ea57f8f0
Barış Can Nazlıcan <64064136+wulfharth7@users.noreply.github.com>
2023-06-12 15:32:28 +0300

    Delete haymatlos_backend directory

--- haymatlos_backend/haymatlos_backend/Hubs/IUserHub.cs
+++
@@ -1,9 +0,0 @@
-﻿using haymatlos_backend.Models;
-
-namespace haymatlos_backend.Hubs
-{
-    public interface IUserHub
-    {
-        Task ShowAllUserswithSignalR(List<UserModel>? users);
-    }
-}
--- haymatlos_backend/haymatlos_backend/Services/dbservices/InterfaceDBservice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace haymatlos_backend.Services.dbservices
-{
-    public interface IdatabaseService
-    {
-        Task<T> GetAsync<T>(string command, object parms);
-        Task<List<T>> GetAll<T>(string command, object parms);
-        Task<int> EditData(string command, object parms);
-    }
-}
