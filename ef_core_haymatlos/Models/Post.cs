f5ee581af8564b6a725010de96a62bc2b1721ee1
Barış Can Nazlıcan <64064136+wulfharth7@users.noreply.github.com>
2023-06-12 15:35:02 +0300

    testing EF Core with Fluent API
    instead of dapper, i'm trying out EF Core here, i've extracted the table to the efcore itself with scaffolding.

---
+++ ef_core_haymatlos/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ef_core_haymatlos.Models;
+
+public partial class Post
+{
+    public string Owner { get; set; } = null!;
+
+    public string? Title { get; set; }
+
+    public string? Text { get; set; }
+
+    public string? Image { get; set; }
+
+    public int Id { get; set; }
+
+    public string Uuid { get; set; } = null!;
+
+    public string[]? Tag { get; set; }
+
+    public int? Like { get; set; }
+
+    public virtual User OwnerNavigation { get; set; } = null!;
+}
---
+++ ef_core_haymatlos/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ef_core_haymatlos.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Uuid { get; set; } = null!;
+
+    public string Nickname { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+}
