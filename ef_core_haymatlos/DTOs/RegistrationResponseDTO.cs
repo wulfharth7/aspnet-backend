e7f335f8511721c1445b80efe97fc3d44fe6d677
Barış Can Nazlıcan <64064136+wulfharth7@users.noreply.github.com>
2023-06-21 16:20:41 +0300

    Delete ef_core_haymatlos directory

--- ef_core_haymatlos/DTOs/RegistrationResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ef_core_haymatlos.DTOs
-{
-    public class RegistrationResponseDTO
-    {
-        public bool IsSuccessfulRegistration { get; set; }
-        public IEnumerable<string>? Errors { get; set; }
-    }
-}
--- ef_core_haymatlos/Models/readme.md
+++
@@ -1,3 +0,0 @@
-# Notes
-
-Lazy Loading in EF Core may cause N+1 problem. Keep an eye on it. (virtual navigation properties in the models)
